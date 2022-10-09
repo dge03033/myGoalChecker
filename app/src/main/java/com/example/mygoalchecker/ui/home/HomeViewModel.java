41a0324c78ec7c8bb760df6d33039304360f0e9e
s_uja <s_ujan@naver.com>
2022-10-09 18:00:03 +0900

    start test

--- app/src/main/java/com/example/mygoalchecker/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/mygoalchecker/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment22222");
+        mText.setValue("This is home fragment222223333");
     }
 
     public LiveData<String> getText() {
